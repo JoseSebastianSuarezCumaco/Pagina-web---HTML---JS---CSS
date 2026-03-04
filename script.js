1c3f1830d58f2b42447f9868c0bf4dd84967c34a
JoseSebastianSuarezCumaco <masquebugs1@gmail.com>
2026-03-04 13:39:44 -0500

    Actualizacion
    Removed comments to clean up the code.

--- script.js
+++ script.js
@@ -1,24 +1,18 @@
-// Funcion para mostrar secciones dinamicamente
 function mostrarSeccion(id) {
 
-    // Oculta todas las secciones
+
     const secciones = document.querySelectorAll(".contenido");
     secciones.forEach(seccion => {
         seccion.style.display = "none";
     });
 
-    // Muestra solo la seleccionada
     document.getElementById(id).style.display = "block";
 }
 
-
-// Mostrar primera seccion al cargar
 document.addEventListener("DOMContentLoaded", function() {
     mostrarSeccion("estructurales");
 });
 
-
-// Validacion basica formulario
 document.addEventListener("DOMContentLoaded", function() {
 
     const formulario = document.getElementById("formulario");
